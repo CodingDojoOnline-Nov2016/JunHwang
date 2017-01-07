69a70e9b215b37c5bc4b0a8963980035470289c1
junhwang <junhwang77@hotmail.com>
2017-01-06 16:40:22 -0800

    the_wall completed

--- virtualenv/venv/RegistrationForm/RegForm.py
+++ virtualenv/venv/RegistrationForm/RegForm.py
@@ -2,18 +2,16 @@ from flask import Flask, render_template, redirect, request, session, flash
 import re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
-
-
 app = Flask(__name__)
 app.secret_key = "ThisIsSecret"
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template("RegForm.html")
 
 @app.route('/process', methods=['POST'])
-def submit():
-    if len(request.form['email']) < 1 or len(request.form['fname']) < 1 or len(request.form['lname']) < 1 or len(request.form['pass']) < 1 or len(request.form['confp']) < 1:
+def create():
+    if len(request.form['email']) < 1 or len(request.form['fname']) < 2 or len(request.form['lname']) < 2 or len(request.form['pass']) < 1 or len(request.form['confp']) < 1:
         flash("All fields are required and must not be blank.", "error")
     elif str.isalpha(str(request.form['fname'])) == False and str.isalpha(str(request.form['lname'])) == False:
         flash("Name fields must contain all alphabetical characters.", "error")
